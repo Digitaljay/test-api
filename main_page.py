ce6bb3b3c00a387488d35b9052d81037ba5eb7fc
mashed-potatoes <37037851+mashed-potatoes@users.noreply.github.com>
2020-01-21 14:52:06 +0300

    Use env port

--- main_page.py
+++ main_page.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 app = Flask(__name__)
 
@@ -10,4 +11,4 @@ def hello():
     return 'data taken'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=os.getenv('PORT', 5000))
