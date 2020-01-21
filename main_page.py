fe1b5a93933fe45898931e86a294e7f1795c7e2b
Digitaljay <pushkareva_lisa@mail.ru>
2020-01-21 14:42:04 +0300

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -1,15 +1,13 @@
 from flask import Flask
-from flask_restful import Api, Resource, reqparse
-import random
 app = Flask(__name__)
-api = Api(app)
-
 
 @app.route('/')
-def show_user_profile(username):
-    return "just main page"
+def hello_world():
+    return 'base page'
 
 @app.route('/take')
-def show_user_profile(username):
-    return "sth from firebase"
+def hello():
+    return 'data taken'
 
+if __name__ == '__main__':
+    app.run()
